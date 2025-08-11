1c1298603e0b5aa66cf03069abd164e1e4383d1f
chaitanya nikam <98545479+chaitanyanikam007@users.noreply.github.com>
2025-08-11 19:13:16 +0530

    all assignments and practicals are done

---
+++ AssignmentOfFactorial.js
@@ -0,0 +1,24 @@
+function facrotial(num){
+     fact=1;
+        for(let i =num; i>=1;i--){
+        fact= i*fact; 
+    }
+           if(num===null || num===undefined){
+        console.log(`Invalid input " Unhappy Scenrio"`);
+    }
+    if(num!=null || num!=undefined){
+        console.log(fact);
+        
+    }
+    
+}
+facrotial(5);
+facrotial(3);
+facrotial(null);
+facrotial(undefined);
+facrotial(9);
+facrotial(0);
+
+
+
+   
